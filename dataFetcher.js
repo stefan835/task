ba0b02a644405d2ad4fdaa493049b13e489603d9
Piotr Stefanowicz <stefan8357@gmail.com>
2017-11-13 22:28:59 +0100

    basic table structure

--- dataFetcher.js
+++ dataFetcher.js
@@ -1,7 +1,8 @@
 fetch('public/data/MOCK_DATA.json')
-  .then( response => response.json()
+  .then(response => response.json()
   )
-  .then(data=>{
+  .then(data => {
     console.log(data)
+
   })
-  .catch(err=>console.log(err));
+  .catch(err => console.log(err));
