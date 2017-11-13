5a2500679e1c8e2a41dcf66fcf4dabe5d2a5559f
Piotr Stefanowicz <stefan8357@gmail.com>
2017-11-13 23:00:54 +0100

    displaying data in table

--- dataFetcher.js
+++ dataFetcher.js
@@ -3,6 +3,14 @@ fetch('public/data/MOCK_DATA.json')
   )
   .then(data => {
     console.log(data)
+    data.map(customer=>{
+      $('.customers--list tbody').append($('<tr>')
+        .append($('<td>').addClass('customer--list-item').text(customer.id))
+        .append($('<td>').addClass('customer--list-item').text(`${customer.first_name} ${customer.last_name}`))
+        .append($('<td>').addClass('customer--list-item').text(customer.phone))
+        .append($('<td>').addClass('customer--list-item').text(customer.age)
+        ))
+    });
 
   })
   .catch(err => console.log(err));
