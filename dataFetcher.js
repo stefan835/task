6ea9e976871b8885e224b46d41d0236ea47a1bb9
Piotr Stefanowicz <stefan8357@gmail.com>
2017-11-14 02:02:40 +0100

    adding bootstrap, adding colors

--- dataFetcher.js
+++ dataFetcher.js
@@ -2,15 +2,16 @@ fetch('public/data/MOCK_DATA.json')
   .then(response => response.json()
   )
   .then(data => {
-    console.log(data)
     data.map(customer=>{
-      $('.customers--list tbody').append($('<tr>')
+      $('.customers--list tbody').append($('<tr>').addClass('customer--list-row')
         .append($('<td>').addClass('customer--list-item').text(customer.id))
         .append($('<td>').addClass('customer--list-item').text(`${customer.first_name} ${customer.last_name}`))
         .append($('<td>').addClass('customer--list-item').text(customer.phone))
         .append($('<td>').addClass('customer--list-item').text(customer.age)
         ))
     });
-
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    $('.customers--list tbody').text('Wystąpił problem z ładowaniem danych. Spróbuj ponownie.');
+    console.log(err)
+  });
